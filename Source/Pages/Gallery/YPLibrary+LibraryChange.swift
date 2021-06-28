5b9941e4ef33a0110d02c5f547beca175978ce7c
袁仕崇 <xiaochong2154@163.com>
2021-06-28 19:53:00 +0800

    IMPROVE: // reset index method

--- Source/Pages/Gallery/YPLibrary+LibraryChange.swift
+++ Source/Pages/Gallery/YPLibrary+LibraryChange.swift
@@ -40,20 +40,20 @@ extension YPLibraryVC: PHPhotoLibraryChangeObserver {
                             if (changedIndexes?.count ?? 0) != 0 {
                                 collectionView.reloadItems(at: changedIndexes!.aapl_indexPathsFromIndexesWithSection(0))
                             }
-                            
-                            //reset index to 0 when selected item changed to fix can't fetch asset from photo library
-                            guard let `self` = self else { return }
-                            let removedIndexItems = collectionChanges?.removedIndexes?.aapl_indexPathsFromIndexesWithSection(0).map({ $0.item }) ?? []
-                            let changedIndexItems = collectionChanges?.changedIndexes?.aapl_indexPathsFromIndexesWithSection(0).map({ $0.item }) ?? []
-                            let allItems = removedIndexItems + changedIndexItems
+                        }
+                        //reset index to 0 when selected item changed to fix can't fetch asset from photo library
+                        guard let `self` = self else { return }
+                        let removedIndexItems = collectionChanges?.removedIndexes?.aapl_indexPathsFromIndexesWithSection(0).map({ $0.item }) ?? []
+                        let changedIndexItems = collectionChanges?.changedIndexes?.aapl_indexPathsFromIndexesWithSection(0).map({ $0.item }) ?? []
+                        let allItems = removedIndexItems + changedIndexItems
 //                            let selectionIndexes = selection.map { $0.index }
 //                            if selectionIndexes.contains(where: { allItems.contains($0)}) {
-                            if !allItems.isEmpty {
-                                self.currentlySelectedIndex = 0
-                                self.selection.removeAll()
-                                self.refreshMediaRequest()
-                            }
+                        if !allItems.isEmpty {
+                            self.currentlySelectedIndex = 0
+                            self.selection.removeAll()
+                            self.refreshMediaRequest()
                         }
+
                     })
                 }
                 self.mediaManager.resetCachedAssets()
